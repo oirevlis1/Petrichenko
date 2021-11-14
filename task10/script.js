90c0534d1197d73781553be69b5d8c8b0029d9cb
Serg <examle@gmail.com>
2021-11-14 10:26:08 +0200

    task11

--- task10/script.js
+++ task10/script.js
@@ -32,58 +32,5 @@ window.addEventListener('DOMContentLoaded', function() {
                 }
             }
         }
-    });
-
-    // Timer
-
-    let deadline = '2021-10-28';
-
-    function getTimeRemaining(endtime) {
-        let t = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor((t/1000) % 60),
-            minutes = Math.floor((t/1000/60) % 60),
-            hours = Math.floor((t/(1000*60*60)));
-
-        return {
-            'total': t,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
-    }
-
-    function setClock(id, endtime) {
-        let timer = document.getElementById(id),
-            hours = timer.querySelector('.hours'),
-            minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds'),
-            timeInterval = setInterval(updateClock, 1000);
-
-        function updateClock() {
-            let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
-            
-            if (t.seconds <= 9) {
-                seconds.textContent = '0' + t.seconds;
-            }
-
-            if (t.minutes <= 9) {
-                minutes.textContent = '0' + t.minutes;
-            }
-
-            if (t.hours <=9) {
-                hours.textContent = '0' + t.hours;
-            }
-
-            if (t.total <= 0) {
-                clearInterval(timeInterval);
-            }
-        }
-
-    }
-
-    setClock('timer', deadline);
+    })
 });
-
